b759639005c376e2ce780475af526e900e33144f
alex210501 <alejandrobrb210501@gmail.com>
2021-11-30 16:02:49 +0100

    Access denied when you're not login

--- controllers/formationController.js
+++ controllers/formationController.js
@@ -3,12 +3,15 @@ let database = require('../database');
 module.exports.formationList = function (req, res) {
 	formationList = [];
 
-	database.query("SELECT * from formation", (error, result) => {
-		if (error) console.log(error);
-		else {
-			console.log(result);
-			formationList = result;
-		}
-		res.render('formationList.ejs', { formations: formationList});
-	});
+    if (req.session.user) { 
+        database.query("SELECT * from formation", (error, result) => {
+            if (error) console.log(error);
+            else {
+                console.log(result);
+                formationList = result;
+            }
+            res.render('formationList.ejs', { formations: formationList});
+        });   
+    }
+    else res.send('Access denied');
 }
