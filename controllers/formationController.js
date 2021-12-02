98c4136d43b9bca2e7ca5d48fbf4e7ed51400374
alex210501 <alejandrobrb210501@gmail.com>
2021-12-02 08:25:53 +0100

    Avoid duplication in subscription

--- controllers/formationController.js
+++ controllers/formationController.js
@@ -17,16 +17,24 @@ module.exports.formationList = function (req, res) {
 }
 
 module.exports.formationSubscribe = function(req, res) {
-    let idFormation = req.params.idformation;
-    let user;
+    let idFormation = parseInt(req.params.idformation);
     
     database.query("SELECT iduser from user WHERE pseudo=?", req.session.user, (error, result) => {
-            if (error) console.log(error);
-            else {
-                let idUser = result[0].iduser;
-                database.query("INSERT INTO subscription (iduser, idformation) VALUES (?,?)",
-                              [idUser, idFormation], (error, result) => {if (error) console.log(error)});
-            }
-        });
+        if (error) console.log(error);
+        else {
+            let idUser = parseInt(result[0].iduser);
+            
+            // Check if the values are not on the subscription table
+            database.query("SELECT * FROM subscription WHERE iduser=? AND idformation=?", [idUser, idFormation], (error, result) => {
+                if (error) console.log(error);
+                else {
+                    if (result.length == 0) { 
+                        database.query("INSERT INTO subscription (iduser, idformation) VALUES (?,?)",   
+                                      [idUser, idFormation], (error, result) => {if (error) console.log(error)});    
+                    }
+                }
+            });
+        }
+    });
     res.redirect('/formations');
 }
