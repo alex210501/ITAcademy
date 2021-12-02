593a2726d8af4b7d38853479539a5dea286b0b31
alex210501 <alejandrobrb210501@gmail.com>
2021-12-02 08:29:57 +0100

    Add button for view cart and connection

--- controllers/formationController.js
+++ controllers/formationController.js
@@ -3,17 +3,14 @@ let database = require('../database');
 module.exports.formationList = function (req, res) {
 	formationList = [];
 
-    if (req.session.user) { 
-        database.query("SELECT * from formation", (error, result) => {
-            if (error) console.log(error);
-            else {
-                console.log(result);
-                formationList = result;
-            }
-            res.render('formationList.ejs', { formations: formationList});
-        });   
-    }
-    else res.send('Access denied');
+    database.query("SELECT * from formation", (error, result) => {
+        if (error) console.log(error);
+        else {
+            console.log(result);
+            formationList = result;
+        }
+        res.render('formationList.ejs', { formations: formationList});
+    });
 }
 
 module.exports.formationSubscribe = function(req, res) {
